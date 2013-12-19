cce87787d87a5e01918d4683ac91e2ff19f98951
Austin Winslow <awinslow21@gmail.com>
2013-12-19 10:42:34 -0500

    Step 4, add and test rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219152340) do
+ActiveRecord::Schema.define(version: 20131219153206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20131219152340) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
+    t.boolean  "favorite"
+    t.integer  "rating"
   end
 
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe Book do
   let(:blanks) {['', nil]}
-  
+  let(:valid_ratings) {[0, 10, 100]}
+  let(:invalid_ratings) {['', -10, -100, 'abc']}
+
   it {should have_valid(:title).when('a title')}
   it {should_not have_valid(:title).when(*blanks)}
+
+  it {should have_valid(:rating).when(*valid_ratings)}
+  it {should_not have_valid(:rating).when(*invalid_ratings)}
 end
