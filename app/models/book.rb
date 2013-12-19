0cbefef74e952d979b32d5a0a441f53efe859720
Austin Winslow <awinslow21@gmail.com>
2013-12-19 10:56:22 -0500

    Step 6, create checkouts & associate with books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
   validates_presence_of :title
   validates_numericality_of :rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
+  has_many :checkouts
 end
---
+++ spec/models/checkout_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Checkout do
+  it {should belong_to :book}
+end
