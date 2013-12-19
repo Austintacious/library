9c299ea8138d4ff14ec1f6149417b3605d5596a2
Austin Winslow <awinslow21@gmail.com>
2013-12-19 13:45:12 -0500

    Step 9, added a few more tests

--- app/models/categorizations.rb
+++ app/models/categorizations.rb
@@ -1,4 +1,5 @@
 class Categorizations < ActiveRecord::Base
   belongs_to :book
   belongs_to :category
+  validates_presence_of :book_id, :category_id
 end
--- spec/models/categorizations_spec.rb
+++ spec/models/categorizations_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe Categorizations do
+  let(:blanks) {['',nil]}
   it {should belong_to :book}
   it {should belong_to :category}
+
+  it {should have_valid(:book_id).when(1)}
+  it {should_not have_valid(:book_id).when(*blanks)}
+
+  it {should have_valid(:category_id).when(1)}
+  it {should_not have_valid(:category_id).when(*blanks)}
 end
